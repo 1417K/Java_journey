6e20e0e491c0f3a5338cfe74c220eb2ab5613d19
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:36:27 +0530

    Add files via upload

---
+++ long.java
@@ -0,0 +1,19 @@
+
+
+// long size = 8 bytes
+
+
+class LongDemo{
+
+	public static void main(String[]args){
+
+		long l = 214750000;
+
+		System.out.println(l);
+		
+		long m = -214750000;
+
+		System.out.println(m);
+	}
+
+}
