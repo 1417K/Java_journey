43225c3fe40f16e7e5daff706d81cd1cd850a4e7
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:35:20 +0530

    Add files via upload

---
+++ short.java
@@ -0,0 +1,22 @@
+
+
+// Short range = -32768 to +32767 
+// short size = 2 bytes (16 bits)
+
+
+class ShortDemo{
+
+	public static void main(String[]args){
+
+
+		short s = 32767;
+
+		System.out.println(s);
+		
+		short p = -32768;
+		
+		System.out.println(p);
+
+
+	}
+}
