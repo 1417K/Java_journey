2327a61c89f284aa1062ade9941846836d36a4ae
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:39:12 +0530

    Add files via upload

---
+++ byte.java
@@ -0,0 +1,16 @@
+
+
+// Byte Range = -128 to +127
+// size = 1 byte(8 bit) 
+
+class ByteDemo{
+
+	public static void main(String[]args){
+
+		byte b=-128;
+		System.out.println(b);
+
+		byte c=127;
+		System.out.println(c);
+	}
+}
