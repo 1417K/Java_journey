645bf17692a6c9fb79b1b687b1c9978be6b23d74
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:38:49 +0530

    Add files via upload

---
+++ char.java
@@ -0,0 +1,21 @@
+
+
+// char size = 2 bytes
+
+class CharDemo{
+
+	public static void main(String[]args){
+
+		char ch = 65;
+		
+		System.out.println(ch);
+		
+		char ch1 = 'A';
+		
+		System.out.println(ch1);
+		
+		char ch2 = '\n';
+		
+		System.out.println(ch2);
+	}
+}
