158e3898529f181e465f0a0d32cc2b4d17937b57
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:38:28 +0530

    Add files via upload

---
+++ double.java
@@ -0,0 +1,22 @@
+
+
+//double size = 8 bytes
+
+
+class DoubleDemo{
+
+	public static void main(String[]args){
+
+		double d =10000000;
+
+		System.out.println(d);
+		
+		double e =0.10000000;
+
+		System.out.println(e);
+		
+		double f =10000000f;
+
+		System.out.println(f);
+	}
+}
