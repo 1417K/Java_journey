3cdffc94649259c2fbc5d19ce2ff3bea2f25e428
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:39:45 +0530

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,18 @@
+
+
+
+//boolean size = 1 bit
+
+class BoolDemo{
+
+	public static void main(String[]args){
+
+		boolean b1 = true ;
+
+		System.out.println(b1);
+		
+		boolean b2 = false ;
+
+		System.out.println(b2);
+	}
+}
