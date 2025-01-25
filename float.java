47b6092472233300989e15b3052d3cc07660684c
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:38:04 +0530

    Add files via upload

---
+++ float.java
@@ -0,0 +1,18 @@
+
+// Float range 
+// float size = 4 bytes
+
+
+class FloatDemo{
+
+	public static void main(String[]args){
+
+		float f = 2.147900000f;
+
+		System.out.println(f);
+		
+		float g = 2140000;
+
+		System.out.println(g);
+	}
+}
