0e0dc6a8edb766636b754427f1a0d2d71d5e16b1
Komal Pawar <152048397+1417K@users.noreply.github.com>
2025-01-25 10:37:38 +0530

    Add files via upload

---
+++ int.java
@@ -0,0 +1,19 @@
+
+
+// Int range = 214 cr 74 lakhs
+// int size = 4 bytes
+
+
+class IntDemo{
+
+	public static void main(String[]args){
+
+		int a = 2147400000;
+
+		System.out.println(a);
+		
+		int b = -2147400000;
+
+		System.out.println(b);
+	}
+}
